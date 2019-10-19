83e6bbed5363f1edc7b293fd30e160f4e61c6861
Noah Loomans <noahloomans@gmail.com>
2019-10-19 22:41:12 +0200

    fix python -m ft_turing

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="ft_turing",
     version="0.1.0",
-    packages=find_packages(),
+    packages=['ft_turing'],
     install_requires=['docopt>=0.6.2'],
     entry_points={
-        'console_script': [
-            'ft_turing = ft_turing:main'
+        'console_scripts': [
+            'ft_turing = ft_turing.__main__:main',
         ]
     }
 )
