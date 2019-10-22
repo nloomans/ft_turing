f5b613e7402565c04dd46949f1fd5002ff8a3349
Noah Loomans <noahloomans@gmail.com>
2019-10-22 14:44:46 +0200

    add missing colorama dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="ft_turing",
     version="0.1.0",
     packages=['ft_turing'],
-    install_requires=['docopt>=0.6.2'],
+    install_requires=['docopt>=0.6.2', 'colorama>=0.4.1'],
     entry_points={
         'console_scripts': [
             'ft_turing = ft_turing.__main__:main',
